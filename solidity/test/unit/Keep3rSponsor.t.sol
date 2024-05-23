ba0bd4c7d4015a84bd16bced81fe250898239c1e
Ashitaka <ashitaka@defi.sucks>
2024-05-23 14:43:17 +0200

    fix: remove ownable tests

--- solidity/test/unit/Keep3rSponsor.t.sol
+++ solidity/test/unit/Keep3rSponsor.t.sol
@@ -46,8 +46,6 @@ contract Keep3rSponsorForTest is Keep3rSponsor {
 contract Keep3rSponsorUnitTest is Test {
   // Events
   event JobExecuted(address _job);
-  event ChangeOwner(address indexed _pendingOwner);
-  event AcceptOwner(address indexed _owner);
   event FeeRecipientSetted(address indexed _feeRecipient);
   event OpenRelaySetted(IOpenRelay indexed _openRelay);
   event ApproveSponsoredJob(address indexed _job);
@@ -85,68 +83,6 @@ contract Keep3rSponsorUnitTest is Test {
   function _revertOnlyOwner() internal {
     vm.expectRevert(abi.encodeWithSelector(IOwnable.Ownable_OnlyOwner.selector));
     changePrank(pendingOwner);
-  }
-}
-
-contract UnitKeep3rSponsorChangeOwner is Keep3rSponsorUnitTest {
-  function setUp() public override {
-    Keep3rSponsorUnitTest.setUp();
-
-    vm.startPrank(owner);
-  }
-
-  function testRevertIfCallerIsNotOwner() public {
-    _revertOnlyOwner();
-    keep3rSponsor.changeOwner(pendingOwner);
-  }
-
-  function testSetPendingOwner() public {
-    keep3rSponsor.changeOwner(pendingOwner);
-
-    assertEq(keep3rSponsor.pendingOwner(), pendingOwner);
-  }
-
-  function testEmitChangeOwner() public {
-    vm.expectEmit();
-    emit ChangeOwner(pendingOwner);
-
-    keep3rSponsor.changeOwner(pendingOwner);
-  }
-}
-
-contract UnitKeep3rSponsorAcceptOwner is Keep3rSponsorUnitTest {
-  function setUp() public override {
-    Keep3rSponsorUnitTest.setUp();
-
-    keep3rSponsor.setPendingOwnerForTest(pendingOwner);
-
-    vm.startPrank(pendingOwner);
-  }
-
-  function testRevertIfCallerIsNotPendingOwner() public {
-    vm.expectRevert(abi.encodeWithSelector(IOwnable.Ownable_OnlyPendingOwner.selector));
-
-    changePrank(owner);
-    keep3rSponsor.acceptOwner();
-  }
-
-  function testSetJobOwner() public {
-    keep3rSponsor.acceptOwner();
-
-    assertEq(keep3rSponsor.owner(), pendingOwner);
-  }
-
-  function testDeletePendingOwner() public {
-    keep3rSponsor.acceptOwner();
-
-    assertEq(keep3rSponsor.pendingOwner(), address(0));
-  }
-
-  function testEmitAcceptOwner() public {
-    vm.expectEmit();
-    emit AcceptOwner(pendingOwner);
-
-    keep3rSponsor.acceptOwner();
   }
 }
 
