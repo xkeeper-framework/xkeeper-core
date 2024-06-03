78c2518175e5d3f78c6d375609045768fea24734
Ashitaka <ashitaka@defi.sucks>
2024-06-03 16:00:03 -0300

    fix: fuzz not work

--- solidity/test/unit/AutomationVaultFactory.t.sol
+++ solidity/test/unit/AutomationVaultFactory.t.sol
@@ -74,24 +74,26 @@ contract UnitAutomationVaultFactoryGetTotalAutomationVaults is AutomationVaultFa
 }
 
 contract UnitAutomationVaultFactoryGetAutomationVaults is AutomationVaultFactoryUnitTest {
-  modifier happyPath(address[] memory _automationVaults, uint256 _startFrom, uint256 _amount) {
+  using EnumerableSet for EnumerableSet.AddressSet;
+
+  EnumerableSet.AddressSet internal _cleanAutomationVaults;
+
+  modifier happyPath(address[] memory _automationVaults) {
     // Avoid underflow
-    vm.assume(_startFrom < _automationVaults.length);
-    vm.assume(_amount < _automationVaults.length - _startFrom);
+    vm.assume(_automationVaults.length > 30);
 
     automationVaultFactory.addAutomationVaultForTest(_automationVaults);
+
+    for (uint256 _index; _index < _automationVaults.length; _index++) {
+      _cleanAutomationVaults.add(_automationVaults[_index]);
+    }
     _;
   }
 
-  function testGetAutomationVaults(
-    address[] memory _automationVaults,
-    uint256 _startFrom,
-    uint256 _automationVaultAmount
-  ) public happyPath(_automationVaults, _startFrom, _automationVaultAmount) {
-    vm.assume(_automationVaultAmount < 30);
-    address[] memory __automationVaults = automationVaultFactory.automationVaults(_startFrom, _automationVaultAmount);
+  function testGetAutomationVaults(address[] memory _automationVaults) public happyPath(_automationVaults) {
+    address[] memory __automationVaults = automationVaultFactory.automationVaults(0, _cleanAutomationVaults.length());
 
-    assertEq(__automationVaults.length, _automationVaultAmount);
+    assertEq(_cleanAutomationVaults.length(), __automationVaults.length);
   }
 }
 
